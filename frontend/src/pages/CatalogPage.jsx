5ae517a28cc0437f134e2118f9fa7f2461114ffb
DmchFast <chdima28@gmail.com>
2026-05-10 15:23:34 +0300

    feat: add basic pages

---
+++ frontend/src/pages/CatalogPage.jsx
@@ -0,0 +1,8 @@
+import { useState } from 'react'
+
+
+function CatalogPage() {
+  return <div>Каталог марок</div>
+}
+
+export default CatalogPage
---
+++ frontend/src/pages/UsersPage.jsx
@@ -0,0 +1,7 @@
+import { useState } from 'react'
+
+function UsersPage() {
+  return <div>Пользователи</div>
+}
+
+export default UsersPage
